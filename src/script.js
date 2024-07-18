2570461c83876940ef1fb37ade5493c47e2ad083
= <=>
2024-07-18 11:40:02 +0700

    trending-today-size

--- src/script.js
+++ src/script.js
@@ -13,16 +13,24 @@ $(document).ready(function () {
   });
 
   $(".p-md-5").hide();
-  $(".p-md-5").fadeIn(3000); // Animasi fadeIn dengan durasi 1000ms (1 detik)
+  $(".p-md-5").fadeIn(3000);
   $(".p-md-5").hover(
     function () {
-      $(this).css("background-color", "#666666"); // Mengubah warna background saat hover
-      $(this).fadeIn(); // Efek fade in saat mouse masuk
+      $(this).css("background-color", "#666666");
+      $(this).fadeIn();
     },
     function () {
-      $(this).css("background-color", ""); // Mengembalikan warna background saat mouse keluar
+      $(this).css("background-color", "");
     }
   );
 
   $(".trending-today").hide().fadeIn(4000);
+  $(".trending-today").hover(
+    function () {
+      $(this).css("font-size", "2.5em");
+    },
+    function () {
+      $(this).css("font-size", "");
+    }
+  );
 });
